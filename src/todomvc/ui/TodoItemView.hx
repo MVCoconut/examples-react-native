d2702eec969610ed4021b1c5af22a95c0b6bd1a4
Kevin Leung <kevinresol@gmail.com>
2017-06-05 20:06:22 +0800

    Add some styles

--- src/todomvc/ui/TodoItemView.hx
+++ src/todomvc/ui/TodoItemView.hx
@@ -9,8 +9,21 @@ using StringTools;
 class TodoItemView extends coconut.ui.View<{ item: TodoItem, ondeleted: Void->Void }> {
   
   static var styles = StyleSheet.create({
+    row: {
+      padding: 10,
+      flexDirection: 'row',
+      alignItems: 'center',
+    },
     input: {
       height: 30,
+    },
+    description: {
+      marginLeft: 20, 
+      flex: 1,
+    },
+    delete: {
+      fontWeight: 'bold',
+      color: 'red',
     }
   });
   
@@ -25,13 +38,13 @@ class TodoItemView extends coconut.ui.View<{ item: TodoItem, ondeleted: Void->Vo
       }
 
     return @hxx '
-      <View>
+      <View style=${styles.row}>
         <Switch value={item.completed} onValueChange={v => item.completed = v} />
         <if {isEditing}>
           <TextInput style=${styles.input} value={item.description} onChangeText={v => edit(v)} onBlur={_ => isEditing = false} />
         <else>
-          <Text onPress={_ => this.isEditing = true}>{item.description}</Text>
-          <TouchableHighlight onPress={ondeleted}><View><Text>Delete</Text></View></TouchableHighlight>
+          <Text style=${styles.description} onPress={_ => this.isEditing = true}>{item.description}</Text>
+          <TouchableHighlight onPress={ondeleted}><View><Text style=${styles.delete}>X</Text></View></TouchableHighlight>
         </if>
       </View>
     ';
--- src/todomvc/ui/TodoListView.hx
+++ src/todomvc/ui/TodoListView.hx
@@ -9,34 +9,55 @@ class TodoListView extends coconut.ui.View<{todos:TodoList, filter:TodoFilter}> 
   static var styles = StyleSheet.create({
     app: {
       paddingTop: 20,
+      paddingLeft: 10,
+      paddingRight: 10,
+    },
+    markAllContainer: {
+      flexDirection: 'row',
+      justifyContent: 'center',
     },
     input: {
+      margin: 5,
+      borderWidth: 1,
+      borderColor: 'grey',
       height: 30,
-    }
+    },
+    filterContainer: {
+      marginTop: 20,
+      flexDirection: 'row',
+      justifyContent: 'space-around',
+    },
+    filter: {
+      // flex: 1,
+    },
   });
+  
+  @:state var input:String = '';
   
   function render() '
     <View style=${styles.app}>
       <View>
-        <TextInput style=${styles.input} onSubmitEditing={e => { todos.add(e.nativeEvent.text); e.nativeEvent.text = ""; }} />
-        <if {todos.items.length > 0}>
-          <if {todos.items.exists(TodoItem.isActive)}>
-            <TouchableHighlight onPress={[] => for (i in todos.items) i.completed = true}>
-              <View><Text>Mark all as completed</Text></View>
-            </TouchableHighlight>
-          <else>
-            <TouchableHighlight onPress={[] => for (i in todos.items) i.completed = false}>
-              <View><Text>Unmark all as completed</Text></View>
-            </TouchableHighlight>
+        <TextInput style=${styles.input} value=${input} onChangeText={v => input = v} onSubmitEditing={[] => { todos.add(input); input = ""; }} />
+        <View style=${styles.markAllContainer}>
+          <if {todos.items.length > 0}>
+            <if {todos.items.exists(TodoItem.isActive)}>
+              <TouchableHighlight onPress={[] => for (i in todos.items) i.completed = true}>
+                <View><Text>Mark all as completed</Text></View>
+              </TouchableHighlight>
+            <else>
+              <TouchableHighlight onPress={[] => for (i in todos.items) i.completed = false}>
+                <View><Text>Unmark all as completed</Text></View>
+              </TouchableHighlight>
+            </if>
           </if>
-        </if>
+        </View>
       </View>
 
-        <for {item in todos.items}>
-          <if {filter.matches(item)}>
-            <TodoItemView key={item} item={item} ondeleted={todos.delete.bind(item)} />
-          </if>
-        </for>
+      <for {item in todos.items}>
+        <if {filter.matches(item)}>
+          <TodoItemView key={item} item={item} ondeleted={todos.delete.bind(item)} />
+        </if>
+      </for>
 
       <View>
         <Text>
@@ -45,14 +66,16 @@ class TodoListView extends coconut.ui.View<{todos:TodoList, filter:TodoFilter}> 
             <case {v}>{v} items
           </switch> left
         </Text>
-        <for {f in filter.options}>
-          <TouchableHighlight onPress={[] => filter.toggle(f.value)}>
-            <View><Text>{f.name}</Text></View>
-          </TouchableHighlight>
-        </for>
-        <if {todos.items.exists(TodoItem.isCompleted)}>
-          <!--<TouchableHighlight onPress={todos.clearCompleted}><Text>Clear Completed</Text></TouchableHighlight>
-        </if>
+        <View style=${styles.filterContainer}>
+          <for {f in filter.options}>
+              <TouchableHighlight onPress={[] => filter.toggle(f.value)}>
+                <View style=${styles.filter}><Text>{f.name}</Text></View>
+              </TouchableHighlight>
+          </for>
+          <if {todos.items.exists(TodoItem.isCompleted)}>
+            <!--<TouchableHighlight onPress={todos.clearCompleted}><Text>Clear Completed</Text></TouchableHighlight>
+          </if>
+        </View>
       </View>
     </View>
   ';
